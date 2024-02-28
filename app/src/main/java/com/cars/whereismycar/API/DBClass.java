5b065bcd29489ae6d8b47283cb66294803da282e
mark samulyak <marksamulyak@gmail.com>
2024-02-28 23:20:00 +0300

    Modified: DBHelper.java, DBClass.java (details)

--- app/src/main/java/com/cars/whereismycar/API/DBClass.java
+++ app/src/main/java/com/cars/whereismycar/API/DBClass.java
@@ -5,9 +5,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DBClass {
-    public Cursor userCursor;
-    public SQLiteDatabase db;
-    private int DC;
+    private Cursor userCursor;
+    private SQLiteDatabase db;
     private String geo;
 
     public String getGeo(Context context){
@@ -17,7 +16,6 @@ public class DBClass {
 
         userCursor = db.query(DBHelper.TABLE_USER_VALUES, null, null, null, null, null, null);
         if (userCursor.moveToFirst()) {
-            int IdIndex = userCursor.getColumnIndex(DBHelper.KEY_ID);
             int HashIndex = userCursor.getColumnIndex(DBHelper.KEY_GEO);
             int ActiveIndex = userCursor.getColumnIndex(DBHelper.KEY_ACTIVE);
             do {
--- app/src/main/java/com/cars/whereismycar/API/DBHelper.java
+++ app/src/main/java/com/cars/whereismycar/API/DBHelper.java
@@ -6,13 +6,13 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "userValues";
     public static final String TABLE_USER_VALUES = "user";
 
     public static final String KEY_ID = "_id";
     public static final String KEY_GEO = "geo";
-    public static final String KEY_ACTIVE = "token";
+    public static final String KEY_ACTIVE = "active";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
